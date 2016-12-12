3f5f1598a5f9fa70598a468c1080b130e5b3cbeb
ocefpaf <ocefpaf@gmail.com>
2016-12-12 08:12:53 -0300

    Add skips and no conda-build 2

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [ $(uname) == Darwin ]; then
+    export CC=clang
+    export CXX=clang++
+    export MACOSX_DEPLOYMENT_TARGET="10.9"
+    export CXXFLAGS="-stdlib=libc++ $CXXFLAGS"
+    export CXXFLAGS="$CXXFLAGS -stdlib=libc++"
+fi
+
+
 # Compile
 # -------
 chmod +x configure

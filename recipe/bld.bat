48d4be9a3b2d37003a1e025d703e607e31a1e1e8
Sam Gillingham <gillingham.sam@gmail.com>
2017-02-06 09:37:31 +1000

    try building on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,55 +1,46 @@
-set INCLUDE=%LIBRARY_INC%;%INCLUDE%
-set LIB=%LIBRARY_LIB%;%LIB%
 
-cd qt-everywhere-opensource-src-%PKG_VERSION%
+:: TODO: OpenGL
+set "INCLUDE=%LIBRARY_INC%;%INCLUDE%"
+set "LIB=%LIBRARY_LIB%;%LIB%"
 
-echo y | configure.exe -prefix %LIBRARY_PREFIX% ^
-                       -libdir %LIBRARY_LIB% ^
-                       -bindir %LIBRARY_BIN% ^
-                       -headerdir %LIBRARY_INC%\qt ^
-                       -datadir %LIBRARY_PREFIX% ^
-                       -release ^
-                       -shared ^
-                       -opensource ^
-                       -fast ^
-                       -no-qt3support ^
-                       -nomake examples ^
-                       -nomake demos ^
-                       -nomake docs ^
-                       -openssl ^
-                       -webkit ^
-                       -system-zlib ^
-                       -system-libpng ^
-                       -system-libtiff ^
-                       -system-libjpeg
+:: this needs to be CALLed due to an exit statement at the end of configure:
+call configure ^
+     -prefix %LIBRARY_PREFIX% ^
+     -libdir %LIBRARY_LIB% ^
+     -bindir %LIBRARY_BIN% ^
+     -headerdir %LIBRARY_INC%\qt ^
+     -archdatadir %LIBRARY_PREFIX% ^
+     -datadir %LIBRARY_PREFIX% ^
+     -L %LIBRARY_LIB% ^
+     -I %LIBRARY_INC% ^
+     -confirm-license ^
+     -no-fontconfig ^
+     -icu ^
+     -no-separate-debug-info ^
+     -no-warnings-are-errors ^
+     -nomake examples ^
+     -nomake tests ^
+     -no-warnings-are-errors ^
+     -opengl desktop ^
+     -opensource ^
+     -openssl ^
+     -platform win32-msvc%VS_YEAR% ^
+     -release ^
+     -shared ^
+     -qt-freetype ^
+     -system-libjpeg ^
+     -system-libpng ^
+     -system-zlib
 
-bin\qmake -r QT_BUILD_PARTS="libs tools"
+:: Note - webengine only built when you ask - so we can skip it easily.
+     
+jom -U Release -j%CPU_COUNT%
+if errorlevel 1 exit /b 1
 
-jom -j%NUMBER_OF_PROCESSORS%
-if errorlevel 1 exit 1
-nmake install
-if errorlevel 1 exit 1
-
-rd /S /Q %LIBRARY_PREFIX%\phrasebooks
-rd /S /Q %LIBRARY_PREFIX%\tests
-del %LIBRARY_PREFIX%\q3porting.xml
-
-:: These are here to map cl.exe version numbers, which we use to figure out which
-:: compiler we are using, and which compiler consumers of Qt need to use, to MSVC
-:: year numbers, which is how qt identifies MSVC versions.
-:: Update this with any new MSVC compiler you might use.
-echo @echo 15=2008 >> msvc_versions.bat
-echo @echo 16=2010 >> msvc_versions.bat
-echo @echo 17=2012 >> msvc_versions.bat
-echo @echo 18=2013 >> msvc_versions.bat
-echo @echo 19=2015 >> msvc_versions.bat
-
-for /f "delims=" %%A in ('cl /? 2^>^&1 ^| findstr /C:"Version"') do set "CL_TEXT=%%A"
-FOR /F "tokens=1,2 delims==" %%i IN ('msvc_versions.bat') DO echo %CL_TEXT% | findstr /C:"Version %%i" > nul && set VSTRING=%%j && goto FOUND
-EXIT 1
-:FOUND
-
-mkdir %LIBRARY_PREFIX%\mkspecs\win32-msvc-default
-copy %LIBRARY_PREFIX%\mkspecs\win32-msvc%VSTRING:~0,4%\* %LIBRARY_PREFIX%\mkspecs\win32-msvc-default\
-
+jom -U install -j%CPU_COUNT%
+if errorlevel 1 exit /b 1
+     
+:: To rewrite qt.conf contents per conda environment
 copy "%RECIPE_DIR%\write_qtconf.bat" "%PREFIX%\Scripts\.qt-post-link.bat"
+if errorlevel 1 exit /b 1
+

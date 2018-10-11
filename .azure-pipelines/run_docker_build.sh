3123ac43b2d931a79ec845d8824a52be561bf893
Marius van Niekerk <marius.v.niekerk@gmail.com>
2018-10-11 15:16:09 -0400

    MNT: Re-rendered with conda-smithy 3.1.12.post.dev26 and pinning 2018.10.11

--- .azure-pipelines/run_docker_build.sh
+++ .azure-pipelines/run_docker_build.sh
@@ -8,7 +8,7 @@
 set -xeuo pipefail
 
 THISDIR="$( cd "$( dirname "$0" )" >/dev/null && pwd )"
-PROVIDER_DIR=basename THISDIR
+PROVIDER_DIR=basename $THISDIR
 
 FEEDSTOCK_ROOT=$(cd "$(dirname "$0")/.."; pwd;)
 RECIPE_ROOT="${FEEDSTOCK_ROOT}/recipe"

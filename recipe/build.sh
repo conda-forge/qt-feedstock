8e6904bc57f053f4aac55b7b679c3e601cba792a
Sam Gillingham <gillingham.sam@gmail.com>
2019-02-24 14:08:18 +1000

    ensure path is removed from $CXX etc so that qt mkspecs files do not have build path hard coded in them

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,16 @@ set -ex
 # -------
 chmod +x configure
 
+# Remove the full path from CXX etc. If we don't do this
+# then the full path at build time gets put into 
+# mkspecs/qmodule.pri and qmake attempts to use this.
+export AR=$(basename ${AR})
+export RANLIB=$(basename ${RANLIB})
+export STRIP=$(basename ${STRIP})
+export OBJDUMP=$(basename ${OBJDUMP})
+export CC=$(basename ${CC})
+export CXX=$(basename ${CXX})
+
 # Frustratingly, Qt's configure checks do not honor the standard build
 # environment variables ($CC, $*FLAGS, etc) in a very consistent way. In
 # particular, they are largely ignored during configuration. AFAICT, the best

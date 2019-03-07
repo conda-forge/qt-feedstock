d42167f7ea5d99877580e58357ead23732cfd32b
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2019-03-07 14:56:38 +0000

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -25,30 +25,20 @@ echo y | configure.exe -prefix %LIBRARY_PREFIX% ^
                        -system-zlib ^
                        -system-libpng ^
                        -system-libtiff ^
+                       -L %LIBRARY_LIB% ^
+                       -I %LIBRARY_INC% ^
                        -system-libjpeg
 
 bin\qmake -r QT_BUILD_PARTS="libs tools"
 
-jom -j%NUMBER_OF_PROCESSORS%
+nmake Release
 if errorlevel 1 exit 1
 nmake install
 if errorlevel 1 exit 1
 
-rd /S /Q %LIBRARY_PREFIX%\phrasebooks
-rd /S /Q %LIBRARY_PREFIX%\tests
-del %LIBRARY_PREFIX%\q3porting.xml
-
-:: These are here to map cl.exe version numbers, which we use to figure out which
-:: compiler we are using, and which compiler consumers of Qt need to use, to MSVC
-:: year numbers, which is how qt identifies MSVC versions.
-:: Update this with any new MSVC compiler you might use.
-echo @echo 15=2008 >> msvc_versions.bat
-echo @echo 16=2010 >> msvc_versions.bat
-echo @echo 17=2012 >> msvc_versions.bat
-echo @echo 18=2013 >> msvc_versions.bat
-echo @echo 19=2015 >> msvc_versions.bat
-
-for /f "delims=" %%A in ('cl /? 2^>^&1 ^| findstr /C:"Version"') do set "CL_TEXT=%%A"
+:: To rewrite qt.conf contents per conda environment
+copy "%RECIPE_DIR%\write_qtconf.bat" "%PREFIX%\Scripts\.qt-post-link.bat"
+if errorlevel 1 exit /b 1
 FOR /F "tokens=1,2 delims==" %%i IN ('msvc_versions.bat') DO echo %CL_TEXT% | findstr /C:"Version %%i" > nul && set VSTRING=%%j && goto FOUND
 EXIT 1
 :FOUND

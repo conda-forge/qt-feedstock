ffbd6c5064bc080b986efdb3cf70defc028fe605
Filipe Fernandes <ocefpaf@gmail.com>
2019-05-24 10:00:18 -0300

    MNT: Re-rendered with conda-build 3.18.1, conda-smithy 3.3.4, and conda-forge-pinning 2019.05.21

--- .azure-pipelines/build_steps.sh
+++ .azure-pipelines/build_steps.sh
@@ -30,7 +30,7 @@ run_conda_forge_build_setup
 # "recipe/yum_requirements.txt" file. After updating that file,
 # run "conda smithy rerender" and this line will be updated
 # automatically.
-/usr/bin/sudo -n yum install -y chrpath ruby
+/usr/bin/sudo -n yum install -y mesa-libGL libX11-devel libXt-devel libXext-devel chrpath libXrender-devel gtk2-devel dbus-devel libSM-devel libICE-devel xorg-x11-server-Xvfb ruby
 
 
 # make the build number clobber

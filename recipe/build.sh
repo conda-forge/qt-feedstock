b5ea80d1ee31466bd0d2d34f7c94a8b2475d3ebf
Isuru Fernando <isuruf@gmail.com>
2019-07-25 09:34:46 -0500

    Reduce diff

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,16 @@ rm -f .qmake.stash .qmake.cache || true
 # -------
 chmod +x configure
 
+# Remove the full path from CXX etc. If we don't do this
+# then the full path at build time gets put into
+# mkspecs/qmodule.pri and qmake attempts to use this.
+export AR=$(basename ${AR})
+export RANLIB=$(basename ${RANLIB})
+export STRIP=$(basename ${STRIP})
+export OBJDUMP=$(basename ${OBJDUMP})
+export CC=$(basename ${CC})
+export CXX=$(basename ${CXX})
+
 # Let Qt set its own flags and vars
 for x in OSX_ARCH CFLAGS CXXFLAGS LDFLAGS
 do

129dfffee62d6de40016a1d4286a96482a6e1feb
Julien Schueller <schueller@phimeca.com>
2020-05-14 09:44:53 +0200

    Removed -qt-xcb

--- recipe/build.sh
+++ recipe/build.sh
@@ -179,7 +179,6 @@ if [[ ${HOST} =~ .*linux.* ]]; then
                 -plugin-sql-mysql \
                 -plugin-sql-psql \
                 -qt-pcre \
-                -qt-xcb \
                 -xkbcommon \
                 -dbus \
                 -no-linuxfb \

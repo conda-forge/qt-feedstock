58a9681fe5df0eba845625dd26a269e8551fa321
Julien Schueller <schueller@phimeca.com>
2021-01-06 17:02:06 +0100

    Try qt6

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -2,15 +2,3 @@
 
 set -e
 
-cd test
-ln -s ${GXX} g++
-cp ../xcrun .
-cp ../xcodebuild .
-export PATH=${PWD}:${PATH}
-qmake hello.pro
-make
-./hello
-# Only test that this builds
-make clean
-qmake qtwebengine.pro
-make

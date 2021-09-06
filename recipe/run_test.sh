9b3f067e81fbdaef8b11162f3fa9c2f2d7e93a74
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2021-09-06 16:31:15 -0500

    Update run and test dependencies

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -2,15 +2,14 @@
 
 set -e
 
+ls
 cd test
 ln -s ${GXX} g++
-cp ../xcrun .
-cp ../xcodebuild .
+# cp ../xcrun .
+# cp ../xcodebuild .
 export PATH=${PWD}:${PATH}
 qmake hello.pro
 make
 ./hello
 # Only test that this builds
 make clean
-qmake qtwebengine.pro
-make

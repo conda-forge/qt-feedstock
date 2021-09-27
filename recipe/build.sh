166e34972a6b89965437945faa6b93905d9ed143
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2021-09-27 13:20:10 -0500

    Remove symlinks and xcode-avoidance scripts
    Locally there were no problems without these tweaks.

--- recipe/build.sh
+++ recipe/build.sh
@@ -153,21 +153,6 @@ if [[ $(uname) == "Linux" ]]; then
 fi
 
 if [[ ${HOST} =~ .*darwin.* ]]; then
-
-    # Avoid Xcode
-    cp "${RECIPE_DIR}"/xcrun .
-    cp "${RECIPE_DIR}"/xcodebuild .
-    # Some test runs 'clang -v', but I do not want to add it as a requirement just for that.
-    ln -s "${CXX}" ${HOST}-clang || true
-    # For ltcg we cannot use libtool (or at least not the macOS 10.9 system one) due to lack of LLVM bitcode support.
-    ln -s "${LIBTOOL}" libtool || true
-    # Just in-case our strip is better than the system one.
-    ln -s "${STRIP}" strip || true
-    chmod +x ${HOST}-clang libtool strip
-    # Qt passes clang flags to LD (e.g. -stdlib=c++)
-    export LD=${CXX}
-    PATH=${PWD}:${PATH}
-
     # Because of the use of Objective-C Generics we need at least MacOSX10.11.sdk
     if [[ $(basename $CONDA_BUILD_SYSROOT) != "MacOSX10.12.sdk" ]]; then
       echo "WARNING: You asked me to use $CONDA_BUILD_SYSROOT as the MacOS SDK"

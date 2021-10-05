2c32784257c48ebcf0aa282cb0df9b5b83236fa4
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2021-10-05 13:18:25 -0500

    Build qtcharts in order to preserve parity between *nix and Windows Qt distributions

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -116,6 +116,13 @@ echo Warning %LIBRARY_BIN%\qmake.exe does not exist jom -U install failed, very 
 copy qtbase\bin\qmake.exe %LIBRARY_BIN%\qmake.exe
 :ok_qmake_exists
 
+popd
+pushd qtcharts
+%LIBRARY_BIN%\qmake.exe qtcharts.pro PREFIX=%PREFIX%
+jom
+jom install
+popd
+
 :: To rewrite qt.conf contents per conda environment
 if not exist %PREFIX%\Scripts mkdir %PREFIX%\Scripts
 copy "%RECIPE_DIR%\write_qtconf.bat" "%PREFIX%\Scripts\.qt-post-link.bat"

bbb4e5a0cff5590c526d4d20b6bebf74f693dac8
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2021-10-08 11:21:51 -0500

    Remove qtwebengine test on Windows

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -7,8 +7,3 @@ if %ErrorLevel% neq 0 exit /b 1
 :: Only test that this builds
 nmake clean
 if %ErrorLevel% neq 0 exit /b 1
-qmake qtwebengine.pro
-if %ErrorLevel% neq 0 exit /b 1
-nmake
-if %ErrorLevel% neq 0 exit /b 1
-popd
